6d19a13959a26da448e762c0c5dd7135bc16479e
Nathan Henneman <32675601+NHenneman@users.noreply.github.com>
2019-02-26 10:23:39 -0500

    update SHA512

--- SHA-512/SHA-512/main.cpp
+++ SHA-512/SHA-512/main.cpp
@@ -13,9 +13,9 @@ using namespace std;
 void SetMessageSchedule();
 
 const unsigned int BLOCK_SIZE = 128;			//1024 bits per message block
-const unsigned int WORD_SIZE = 8;				//64 bits per message word
+const unsigned int WORD_SIZE = 64;				//64 bits per message word
 const unsigned int messageWords = 16;			//Number of message words
-vector<string> messageParse;					//Hold all message blocks
+vector<bitset<1024>> messageParse;				//Hold all message blocks
 unsigned long long wordBlocks[messageWords];	//Hold 16 words of current message block
 unsigned long long messageSchedule[80];			//Hold message schedule
 
@@ -33,23 +33,48 @@ int main(int argCount, char *argValues[]) {
 	string contents{ istreambuf_iterator<char>(file), istreambuf_iterator<char>() };
 	
 	int numBlocks = fileSize / 128;
-	int start = 0;
-	int end = BLOCK_SIZE;
+
 	for (int i = 0; i < numBlocks; i++)
 	{
-		start = i * end; 
-		messageParse.push_back(contents.substr(start, end));
+		bitset<1024> w;
+		for (int j = 0; j < BLOCK_SIZE; j++) {
+			char c = contents[i * BLOCK_SIZE + j];
+			for (int k = 0; k <= 7 && c; k++) {
+				if (c & 0x1) {
+					w.set(8 * j + k);
+				}
+				c >>= 1;
+			}
+			//cout << w << endl << endl;
+		}
+		messageParse.push_back(w);
 	}
-	string shortBlock = contents.substr(numBlocks * end);
-	int padding = ((896 % 1024) - ((shortBlock.length() % 128) * 8 + 1) - 7) / 8;
-	shortBlock.append(1, 0x80);
-	shortBlock.append(padding, 0);
-	shortBlock.append(16, 0);
-	messageParse.push_back(shortBlock);
 
-	//cout << padding << endl << shortBlock;
+	string shortBlock = contents.substr(numBlocks * BLOCK_SIZE);
+	bitset<1024> s;
+	for (int i = 0; i < shortBlock.length(); i++)
+	{
+		char c = shortBlock[i];
+		for (int k = 0; k <= 7 && c; k++) {
+			if (c & 0x1) {
+				s.set(8 * i + k);
+			}
+			c >>= 1;
+		}
+	}
+
+	unsigned int padding = ((896 % 1024) - (shortBlock.length() * 8 + 1));
+	s = s << padding;	//Pad message block
+	s.set(padding);		//Set first digit of padding
+	
+	bitset<1024> length(fileSize * 8);
+	s = (s << 128) | length;
+
+	messageParse.push_back(s);
+
 	unsigned long long finalHash[8];
 
+	//Set the message schedule
 	SetMessageSchedule();
 
 	for (int j = 0; j < messageParse.size(); j++)
@@ -64,18 +89,16 @@ int main(int argCount, char *argValues[]) {
 		for (int k = 0; k < messageWords; k++)
 		{
 			bitset<64> b;
+
 			for (int i = 0; i < WORD_SIZE; i++) {
-				char c = messageParse[j][i + k * 8];
-				for (int j = 7; j >= 0 && c; --j) {
-					if (c & 0x1) {
-						b.set(8 * i + j);
-					}
-					c >>= 1;
-				}
+				b[i] = messageParse[j][k * WORD_SIZE + i];
 			}
+			
 			wordBlocks[k] = b.to_ullong();
 		}
 
+		//Perform the Hash calculations for the message
+		/*TODO: check each function for correct implementation*/
 		unsigned long long a, b, c, d, e, f, g, h, t1, t2;
 
 		a = finalHash[0];
@@ -86,28 +109,29 @@ int main(int argCount, char *argValues[]) {
 		f = finalHash[5];
 		g = finalHash[6];
 		h = finalHash[7];
-		for (int t = 1; t <= 79; t++)
+
+		for (int t = 0; t <= 79; t++)
 		{
-			t1 = h + Summation1(e) + Ch(e, f, g) + constants[t] + messageSchedule[t];
-			t2 = Summation0(a) + Maj(a, b, c);
+			t1 = h ^ Summation1(e) ^ Ch(e, f, g) ^ constants[t] ^ messageSchedule[t];
+			t2 = Summation0(a) ^ Maj(a, b, c);
 			h = g;
 			g = f;
 			f = e;
-			e = d + t1;
+			e = d ^ t1;
 			d = c;
 			c = b;
 			b = a;
-			a = t1 + t2;
+			a = t1 ^ t2;
 		}
 
-		finalHash[0] += a;
-		finalHash[1] += b;
-		finalHash[2] += c;
-		finalHash[3] += d;
-		finalHash[4] += e;
-		finalHash[5] += f;
-		finalHash[6] += g;
-		finalHash[7] += h;
+		finalHash[0] ^= a;
+		finalHash[1] ^= b;
+		finalHash[2] ^= c;
+		finalHash[3] ^= d;
+		finalHash[4] ^= e;
+		finalHash[5] ^= f;
+		finalHash[6] ^= g;
+		finalHash[7] ^= h;
 	}
 
 	cout << hex << finalHash[0] << finalHash[1] << finalHash[2] << finalHash[3]
@@ -124,7 +148,7 @@ void SetMessageSchedule()
 		}
 		else
 		{
-			messageSchedule[i] = Sigma1(messageSchedule[i - 2]) + messageSchedule[i - 7] + Sigma0(messageSchedule[i - 15]) + messageSchedule[i - 16];
+			messageSchedule[i] = Sigma1(messageSchedule[i - 2]) ^ messageSchedule[i - 7] ^ Sigma0(messageSchedule[i - 15]) ^ messageSchedule[i - 16];
 
 		}
 	}
